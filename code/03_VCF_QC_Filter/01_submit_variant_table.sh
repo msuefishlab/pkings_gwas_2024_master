97743f5fb85d445f03d705e06336eb7798880faf
Jason Gallant <jgallant@msu.edu>
2023-12-19 12:53:15 -0500

    update code

--- code/03_VCF_QC_Filter/01_submit_variant_table.sh
+++ code/03_VCF_QC_Filter/01_submit_variant_table.sh
@@ -6,11 +6,10 @@
 #!/bin/bash
 
 root="$(git rev-parse --show-toplevel)"
-source $root/"pkings_gwas.env"
-nfiles=$(wc -l < $1)
-inputdata=$1
+source ${root}/"pkings_gwas.env"
+
 
 mkdir -p $root"/output_data/slurm_logs"/03_QC/
 
-echo sbatch --job-name "QC_MAKE_TABLE" --output $root"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=$root,vcf_file=$vcf_file $root/code/03_QC/get_variant_table.sb
-sbatch --job-name "QC_MAKE_TABLE" --output $root"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=$root,vcf_file=$vcf_file $root/code/03_QC/get_variant_table.sb
+echo sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
+sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
