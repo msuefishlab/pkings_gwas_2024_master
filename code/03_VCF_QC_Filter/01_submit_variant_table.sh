fc5eb62b953694cb888a074d2a8b039c1798e9ff
Jason Gallant <jgallant@msu.edu>
2023-12-19 13:22:58 -0500

    parallelize the variant table construction

--- code/03_VCF_QC_Filter/01_submit_variant_table.sh
+++ code/03_VCF_QC_Filter/01_submit_variant_table.sh
@@ -8,8 +8,11 @@
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
+interval_files=($(find "${root}/input_data/01_Terra/chrom-interval-files/" "${root}/input_data/01_Terra/remaining-interval-files/" -type f))
+
+nintervals=${#interval_files[@]}
 
 mkdir -p $root"/output_data/slurm_logs"/03_QC/
 
-echo sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
-sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table.log" --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
+echo sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table_%a.log" -a 0-$nintervals --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
+sbatch --job-name "QC_MAKE_TABLE" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_make_table_%a.log" -a 0-$nintervals --export=root=${root} ${root}/code/03_QC/get_variant_table.sb
