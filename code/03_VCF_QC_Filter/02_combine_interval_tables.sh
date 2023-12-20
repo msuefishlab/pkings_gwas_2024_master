5068e4b5f4637c637e50d1dcc082eea83e9e6e03
Jason Gallant <jgallant@msu.edu>
2023-12-19 22:03:38 -0500

    added merging code

--- code/03_VCF_QC_Filter/02_combine_interval_tables.sh
+++ code/03_VCF_QC_Filter/02_combine_interval_tables.sh
@@ -1,12 +1,12 @@
-## run_upload.sh
+## run_merge.sh
 ## 12-03-23
 ## JRG
-## Submits to queue for variant table construction
+## Merges interval split tab delimited files for QC Analysis
 
 #!/bin/bash
 
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
-awk 'FNR==1 && NR!=1{next;}{print}'${root}/output_data/03_QC/*indel*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.indel.tab
+awk 'FNR==1 && NR!=1{next;}{print}' ${root}/output_data/03_QC/*indel*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.indel.tab
 awk 'FNR==1 && NR!=1{next;}{print}' ${root}/output_data/03_QC/*snp*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.snp.tab
