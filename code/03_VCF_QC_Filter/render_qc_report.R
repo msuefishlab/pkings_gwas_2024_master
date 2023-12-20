ca229be0312a8f3463dbd9687681a1fb7112a30f
Jason Gallant <jgallant@msu.edu>
2023-12-20 17:26:25 -0500

    working render qc report

--- code/03_VCF_QC_Filter/render_qc_report.R
+++ code/03_VCF_QC_Filter/render_qc_report.R
@@ -4,19 +4,18 @@
 #run this script like this:
 # Rscript ${scriptdir}/render_report.R INPUTDATA OUTPUTDATA REPORT.Rmd
 
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 
-root<-rprojroot::find_root(".git/index")
+root <- rprojroot::find_root(".git/index")
 require(yaml)
 require(tools)
 
 
 rmarkdown::render(
-		  input=file.path(args[3]),
-                  output_file= basename(paste0(args[2], ".report.html")),
-		  output_dir = dirname(args[2]),
-		  params=list(
-                  	data_path=args[1]
-		  	)
-		  )
+  input = file.path(args[3]),
+  output_file = basename(paste0(args[2], ".report.html")),
+  output_dir = dirname(args[2]),
+  params = list(
+    data_path = args[1]
+  )
 )
