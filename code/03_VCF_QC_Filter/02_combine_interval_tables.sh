c661bb11595448002cddbf3a68e323c6cf131cad
Jason Gallant <jgallant@msu.edu>
2023-12-19 22:01:04 -0500

    added merging code

---
+++ code/03_VCF_QC_Filter/02_combine_interval_tables.sh
@@ -0,0 +1,12 @@
+## run_upload.sh
+## 12-03-23
+## JRG
+## Submits to queue for variant table construction
+
+#!/bin/bash
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+
+awk 'FNR==1 && NR!=1{next;}{print}'output_data/03_QC/*indel*tab > output_data/03_QC/${vcf_file%.vcf.gz}.all.indel.tab
+awk 'FNR==1 && NR!=1{next;}{print}'output_data/03_QC/*snp*tab > output_data/03_QC/${vcf_file%.vcf.gz}.all.snp.tab
