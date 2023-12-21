eb79e9a325b1f64b740f8a1fd782778938d7d31a
Jason Gallant <jgallant@msu.edu>
2023-12-20 21:04:12 -0500

    updated to use scratch space for large files

--- code/03_VCF_QC_Filter/02_combine_interval_tables.sh
+++ code/03_VCF_QC_Filter/02_combine_interval_tables.sh
@@ -8,5 +8,9 @@
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
-awk 'FNR==1 && NR!=1{next;}{print}' ${root}/output_data/03_QC/*indel*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.indel.tab
-awk 'FNR==1 && NR!=1{next;}{print}' ${root}/output_data/03_QC/*snp*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.snp.tab
+outdir=${scratch_store}/output_data/03_QC/
+
+mkdir -p ${outdir}
+
+awk 'FNR==1 && NR!=1{next;}{print}' ${outdir}/*indel*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.indel.tab
+awk 'FNR==1 && NR!=1{next;}{print}' ${outdir}/*snp*tab > ${root}/output_data/03_QC/${vcf_file%.vcf.gz}.all.snp.tab
