9da653f8c78295880207cfbc143249ccde6f787e
Jason Gallant <jgallant@msu.edu>
2023-12-27 20:12:24 -0500

    updated combine filtered function

---
+++ code/03_VCF_QC_Filter/05_combine_filtered.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## 01_submit_variant_table.sh
+## Feb 2023
+## JRG
+# This script creates a variant table for SNPS and INDELS to determine filter settings.
+
+#!/bin/bash
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+scratchdir=${scratch_store}/output_data/03_QC/
+outdir=${root}/output_data/03_QC/
+
+ls -1 ${scratchdir}*.SNP.ONLY.BIALLELIC.ONLY.filtered.setGT.PASS.vcf.gz > ${outdir}/biallelic_only_shards.txt
+ls -1 ${scratchdir}*.SNP.BIALLELIC.ONLY.AND.INV.TOGETHER.filtered.setGT.PASS.vcf.gz > ${outdir}/biallelic_and_invariant_shards.txt
+ls -1 ${scratchdir}*.INDEL.filtered.setGT.vcf.gz > ${outdir}/indel_shards.txt
+
+cat ${outdir}/biallelic_only_shards.txt > ${outdir}/FILTERED_BIALLELIC_SNPS_ONLY.list
+cat ${outdir}/biallelic_only_shards.txt ${outdir}/indel_shards.txt > ${outdir}/FILTERED_BIALLELIC_AND_INDEL.list
+cat ${outdir}/biallelic_and_invariant_shards.txt > ${outdir}/FILTERED_BIALLELIC_AND_INVARIANT.list
+
+echo sbatch --job-name "COMBINE_FILTERED" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_combined_filtered_%a.log " -a 0-2 --export=root=${root} ${root}/code/03_VCF_QC_Filter/combine_filtered.sb
+sbatch --job-name "COMBINE_FILTERED" --output ${root}"/output_data/slurm_logs"/03_QC/"qc_combined_filtered_%a.log " -a 0-2 --export=root=${root} ${root}/code/03_VCF_QC_Filter/combine_filtered.sb
