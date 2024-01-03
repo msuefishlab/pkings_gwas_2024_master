3df6fa4d05cfcf7a4adfca020ec3741b9507f71c
Jason Gallant <jgallant@msu.edu>
2024-01-02 20:46:05 -0500

    added tajimas d calcs

---
+++ code/05_PopGen/02_run_tajd.sh
@@ -0,0 +1,17 @@
+## 01_submit_variant_table.sh
+## 12-03-23
+## JRG
+## Submits to queue for variant table construction
+
+#!/bin/bash
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+
+interval_files=($(find "${root}/input_data/01_Terra/chrom-interval-files/" "${root}/input_data/01_Terra/remaining-interval-files/" -type f))
+
+nintervals=${#interval_files[@]}
+
+mkdir -p $root"/output_data/slurm_logs"/05_PopGen/
+
+echo sbatch --job-name "POPGEN_TAJD" --output ${root}"/output_data/slurm_logs"/05_PopGen/"POPGEN_TAJD_%a.log" -a 0-$nintervals --export=root=${root} ${root}/code/03_VCF_QC_Filter/tajd.sb
