3d2d2388ff5729127b688454b739120ff6012480
Jason Gallant <jgallant@msu.edu>
2024-01-02 19:38:20 -0500

    updated rename reference

---
+++ code/05_PopGen/00_rename_reference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## 00_rename_reference.sh
+## Jan 2024
+## JRG
+# This script renames the reference fasta based on the chr_name_map for easier analysis
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+
+cat ${root}/input_data/00_Reference_Genome/${chr_name_map} | cut -f1,2 > ${root}/input_data/00_Reference_Genome/input_data/00_Reference_Genome/${chr_name_map}.only.txt
+singularity exec --bind $root:/project_root ${gwas_tools_image}  /seqkit/seqkit replace -p '^(\S+)(.+?)$' -r '{kv}$2' -k /project_root/input_data/00_Reference_Genome/${chr_name_map}.only.txt /project_root/input_data/00_Reference_Genome/${reference} > /project_root/input_data/00_Reference_Genome/${reference}.renamed.fa
