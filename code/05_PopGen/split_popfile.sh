0493fde138ff40ac20178984b896b0412036602b
Jason Gallant <jgallant@msu.edu>
2024-01-02 20:38:16 -0500

    updated split_popfile

--- code/05_PopGen/split_popfile.sh
+++ code/05_PopGen/split_popfile.sh
@@ -12,8 +12,18 @@ output_dir="$2"
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
+# Initialize an associative array to track unique column 2 values
+declare -A unique_pops
+
 # Read the file line by line
 while IFS=$'\t' read -r col1 col2 || [ -n "$col1" ]; do
     # Write the first column value to a file in the specified output directory
     echo "$col1" >> "${output_dir}/${col2}.txt"
+    # Add the value to the associative array
+    unique_pops["$col2"]=1
 done < "$input_file"
+
+# Write the unique column 2 values to unique_pops.txt
+for pop in "${!unique_pops[@]}"; do
+    echo "$pop"
+done > "${output_dir}/unique_pops.txt"
