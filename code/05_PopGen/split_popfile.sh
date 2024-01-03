a07d8c854bfefde39a3a62a005370bf3ef5c7926
Jason Gallant <jgallant@msu.edu>
2024-01-02 20:35:47 -0500

    updated split_popfile

--- code/05_PopGen/split_popfile.sh
+++ code/05_PopGen/split_popfile.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Check if the input file is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <filename>"
+# Check if the input file and output directory are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_directory>"
     exit 1
 fi
 
+input_file="$1"
+output_dir="$2"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
 # Read the file line by line
 while IFS=$'\t' read -r col1 col2 || [ -n "$col1" ]; do
-    # Write the first column value to a file named by the second column value
-    echo "$col1" >> "${col2}.txt"
-done < "$1"
+    # Write the first column value to a file in the specified output directory
+    echo "$col1" >> "${output_dir}/${col2}.txt"
+done < "$input_file"
