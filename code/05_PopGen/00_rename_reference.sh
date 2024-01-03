f76e42a569a61b5c4fb980beef78941368e2509f
Jason Gallant <jgallant@msu.edu>
2024-01-02 19:40:11 -0500

    fixed typo

--- code/05_PopGen/00_rename_reference.sh
+++ code/05_PopGen/00_rename_reference.sh
@@ -8,5 +8,5 @@
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
-cat ${root}/input_data/00_Reference_Genome/${chr_name_map} | cut -f1,2 > ${root}/input_data/00_Reference_Genome/input_data/00_Reference_Genome/${chr_name_map}.only.txt
+cat ${root}/input_data/00_Reference_Genome/${chr_name_map} | cut -f1,2 > ${root}/input_data/00_Reference_Genome/${chr_name_map}.only.txt
 singularity exec --bind $root:/project_root ${gwas_tools_image}  /seqkit/seqkit replace -p '^(\S+)(.+?)$' -r '{kv}$2' -k /project_root/input_data/00_Reference_Genome/${chr_name_map}.only.txt /project_root/input_data/00_Reference_Genome/${reference} > /project_root/input_data/00_Reference_Genome/${reference}.renamed.fa
