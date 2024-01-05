964b176de878cbab0cc506ee4940e9c28e398510
Jason Gallant <jgallant@msu.edu>
2024-01-05 15:14:27 -0500

    fixed file paths in prep data

--- code/06_Association/01_run_prep_data.sh
+++ code/06_Association/01_run_prep_data.sh
@@ -4,20 +4,16 @@
 ## January 2024
 ## JRG
 # This script prepares a VCF files for a set of individuals specified in arg1 with phenotypes in arg2 for relationship matrix construction and association analysis.
-# e.g. bash 01_run_prep_data.sh ./apa_bam_wobs_only.txt ./apa_bam_wobs_only_phenos.txt
-# note that output is names based on the keep file.
+# e.g. bash 01_run_prep_data.sh APA_BAM_ONLY_BP0_TP1_WOB9
 
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
 mkdir -p ${root}/output_data/slurm_logs/06_Assocation/
 
-keep_path=$(realpath $1)
-pheno_path=$(realpath $2)
+$OUTNAME=$1
 
 keepfilename=$(basename $keep_path)
 
-OUTNAME=${keepfilename%.txt}
-
-echo sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_prep_data.log" --export=root=${root} ${root}/code/06_Association/prep_data.sb $keep_path $pheno_path $OUTNAME
-sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_prep_data.log" --export=root=${root} ${root}/code/06_Association/prep_data.sb $keep_path $pheno_path $OUTNAME
+echo sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/ASSOC_"${OUTNAME}"_prep_data.log" --export=root=${root},OUTNAME=${OUTNAME} ${root}/code/06_Association/prep_data.sb
+echo sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/ASSOC_"${OUTNAME}"_prep_data.log" --export=root=${root},OUTNAME=${OUTNAME} ${root}/code/06_Association/prep_data.sb
