591a4070584291d2271b16ed8200d69700347ec1
Jason Gallant <jgallant@msu.edu>
2024-01-05 19:09:54 -0500

    updated paths for relmat

--- code/06_Association/02_run_rel_mat.sh
+++ code/06_Association/02_run_rel_mat.sh
@@ -12,12 +12,7 @@ source ${root}/"pkings_gwas.env"
 
 mkdir -p ${root}/output_data/slurm_logs/06_Assocation/
 
-keep_path=$(realpath $1)
-pheno_path=$(realpath $2)
+OUTNAME=$1
 
-keepfilename=$(basename $keep_path)
-
-OUTNAME=${keepfilename%.txt}
-
-echo sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_MAKE_RELMAT.slurm.log" --export=root=${root} ${root}/code/06_Association/relate_matrix.sb $keep_path $pheno_path $OUTNAME
-sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_MAKE_RELMAT.slurm.log" --export=root=${root} ${root}/code/06_Association/relate_matrix.sb $keep_path $pheno_path $OUTNAME
+echo sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_MAKE_RELMAT.slurm.log" --export=root=${root},OUTNAME=${OUTNAME} ${root}/code/06_Association/relate_matrix.sb
+sbatch --job-name $OUTNAME"_MAKE_RELMAT" --output ${root}"/output_data/slurm_logs/06_Assocation/gemma/ASSOC_"${OUTNAME}"_MAKE_RELMAT.slurm.log" --export=root=${root},OUTNAME=${OUTNAME} ${root}/code/06_Association/relate_matrix.sb
