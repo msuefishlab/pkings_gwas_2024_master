8ebdf5cb83ba506aa88b531e65aaa4fe33557018
Jason Gallant <jgallant@msu.edu>
2024-01-09 13:27:34 -0500

    added run admixture and associated scripts

---
+++ code/05_PopGen/06_run_admixture.sh
@@ -0,0 +1,28 @@
+cd $root
+
+source $root/pkings_gwas.env
+
+MYNAME=$(whoami)
+
+indir=${root}/output_data/04_Phylogeny/
+outdir=${root}/output_data/05_PopGen/
+
+mkdir -p ${outdir}
+mkdir -p ${tmpoutdir}
+
+OUTROOT=${snps_only_vcf%".vcf.gz"}
+
+
+MYNAME=$(whoami)
+
+singularity exec --bind $root:/project_root --bind $outdir:/out_dir --bind $indir:/in_dir --bind $tmpoutdir:/tmp_dir ${gwas_tools_image} R --slave --vanilla --file="/project_root/code/05_PopGen/convert_gds_to_plink.R" --args -i "/indir/${OUTROOT}.renamed.maf10.miss10.dp5.gds" -o "/outdir/${OUTROOT}.renamed.maf10.miss10.dp5.filtered"
+
+cat ${outdir}/{OUTROOT}.renamed.maf10.miss10.dp5.filtered.fam | cut -f2 | awk 'BEGIN { FS = "_" } ; { print $1"_"$2,"unk",$1 }' > ${outdir}/indfile.txt
+
+numk=$(cat ${outdir}/indfile.txt | cut -d" " -f3 | uniq | wc -l)
+
+for K in $(seq 1 $numk); do
+    CMD="singularity exec --bind $root:/project_root --bind $outdir:/out_dir --bind $indir:/in_dir ${gwas_tools_image} /admixture/admixture --cv -j8 /out_dir/${OUTROOT}.renamed.maf10.miss10.dp5.filtered.bed $K | tee /out_dir/{OUTROOT}.renamed.maf10.miss10.dp5.filtered.${K}.out" #Normally you should give --cv as first option to admixture
+    echo sbatch --time=04:00:00 -c 8 -J ADMIXTURE_${K} -o ${root}"/output_data/slurm_logs/05_PopGen/popgen_ADMIXTURE_${K}.log" --export=root=${root} --mem 12000 --wrap="$CMD"
+    sbatch --time=04:00:00 -c 8 -J ADMIXTURE_${K} -o ${root}"/output_data/slurm_logs/05_PopGen/popgen_ADMIXTURE_${K}.log" --export=root=${root} --mem 12000 --wrap="$CMD"
+done
---
+++ code/05_PopGen/convert_gds_to_plink.R
@@ -0,0 +1,40 @@
+library(getopt)
+getwd()
+source('scripdir/04_Windowed_Popstats/gdsSubset.R')
+
+h <- function(x) {
+    cat("Usage: Rscript --vanilla convert_GDS_to_PLINK.R -i VCF_FILE -o PLINK_FILE_PREFIX [-h]\n\n")
+    quit(save="no", status=x)
+}
+
+opt <- getopt(matrix(c(
+    'help',   'h', 0, "logical",
+    'output', 'o', 1, "character",
+    'input', 'i', 1, "character"
+), ncol=4, byrow=TRUE));
+
+if (! is.null(opt$help)) { h(0) }
+
+file.ouptut <- ifelse(is.null(opt$input), "output", opt$output)
+
+if (! is.null(opt$input)) {
+    gds.file <- opt$input
+    if (! file.exists(gds.file)) { cat(sprintf("GDS file (%s) was not found!\n", gds.file)); h(1) }
+} else {
+    h(1)
+}
+
+require(SNPRelate)
+
+gdsfile<-snpgdsOpen(gds.file)
+sampID <- read.gdsn(index.gdsn(gdsfile, "sample.id"))
+sampID_rm<-sampID[!grepl("PMAG|PHOP",sampID)]
+snpgdsClose(gdsfile)
+
+gdsSubset(opt$input,'/mnt/gs18/scratch/users/jgallant/admixture/outgroups.dropped.gds',sample.include = sampID_rm)
+
+gdsfile_new<-snpgdsOpen('/mnt/gs18/scratch/users/jgallant/admixture/outgroups.dropped.gds')
+snpset<-snpgdsLDpruning(gdsfile_new,ld.threshold=0.1,maf=0.1,missing.rate=0.1,num.thread=2)
+snpset.id<-unlist(snpset)
+snpgdsGDS2BED(gdsfile_new, opt$output, sample.id=NULL, snp.id=snpset.id, snpfirstdim=NULL,verbose=TRUE)
+snpgdsClose(gdsfile_new)
