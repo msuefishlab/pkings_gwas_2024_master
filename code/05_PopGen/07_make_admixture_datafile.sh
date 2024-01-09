2a925b862cce45b021d43fd11d93fc1e2cdf4be3
Jason Gallant <jgallant@msu.edu>
2024-01-09 17:37:47 -0500

    added admixture datafilemaker

---
+++ code/05_PopGen/07_make_admixture_datafile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## 07_make_admixture_datafile.sh
+## Jan 2024
+## JRG
+# This script combines the ADMIXTURE datafiles for the different K clusters
+
+root="$(git rev-parse --show-toplevel)"
+
+source $root/pkings_gwas.env
+
+MYNAME=$(whoami)
+
+indir=${root}/output_data/04_Phylogeny/
+outdir=${root}/output_data/05_PopGen/
+
+mkdir -p ${outdir}
+
+OUTROOT=${snps_only_vcf%".vcf.gz"}
+
+rm -f ${outdir}/cv_errors.txt
+grep -h CV ${outdir}/${OUTROOT}.renamed.maf10.miss10.dp5.filtered.*.out | cut -d"=" -f2 | sed --expression='s/):/\t/g' > ${outdir}/cv_errors.txt
+
+
+rm -f ${outdir}/admixture_long_results.txt
+for i in $(cut -f1 ${outdir}/cv_errors.txt);
+  do
+   for k in $(seq 1 $i);
+	do
+      cat  ${outdir}/${OUTROOT}.renamed.maf10.miss10.dp5.filtered.${i}.Q | cut -d" " -f$k | awk -v i="$i" -v k="$k" '{print i,k,$0}'| paste ${outdir}/indfile.txt - >> ${outdir}/admixture_long_results.txt
+   done
+done
+
+echo -e "Sample\tSex\tPop\tMAXK\tGROUP\tQ" | cat - ${outdir}/admixture_long_results.txt > ${outdir}/admixture_long_results.header.txt
+sed -i 's/ /\t/g' ${outdir}/admixture_long_results.header.txt
