9a45dc14ed716f5c299814187a10daf739f5f950
Jason Gallant <jgallant@msu.edu>
2024-01-13 11:23:34 -0500

    added run iqtree

---
+++ code/04_Phylogeny/04_Run_IQTree.sh
@@ -0,0 +1,14 @@
+## 03_Run_Prep_For_IQTree.sh
+## Dec 2023
+## JRG
+## Submits to queue for Phylogeny Reconstruction
+
+#!/bin/bash
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+
+mkdir -p ${root}/output_data/slurm_logs/04_Phylogeny/
+
+echo sbatch --job-name "PHYLO_PREP_IQ_TREE" --output ${root}"/output_data/slurm_logs/04_Phylogeny/phylo_run_iqtree.log" --export=root=${root} ${root}/code/04_Phylogeny/run_iqtree.sb
+sbatch --job-name "PHYLO_PREP_IQ_TREE" --output ${root}"/output_data/slurm_logs/04_Phylogeny/phylo_run_iqtree.log" --export=root=${root} ${root}/code/04_Phylogeny/run_iqtree.sb
