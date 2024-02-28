f197630f41d855bb1238dfab53bbf0cbae8f1a2e
Jason Gallant <jgallant@msu.edu>
2024-02-28 15:57:00 -0500

    peak analysis code updated

---
+++ code/07_Peak_Analysis/02_Render_Peak_Haplotype_Plots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --login
+
+OUTNAME=$1
+
+root="$(git rev-parse --show-toplevel)"
+
+source $root/pkings_gwas.env
+
+outdir=${root}/output_data/07_Peak_Analysis/${OUTNAME}
+
+mkdir -p ${outdir}
+
+# Rscript ${scriptdir}/render_report.R INPUTDATA OUTPUTDATA REPORT.Rmd
+singularity exec ${gwas_tools_image} Rscript ${root}/code/00_common/render_rmd_report.R ${OUTNAME} ${outdir}/${OUTNAME}_Peak_Analysis ${root}/code/06_Association/peak_analysis.Rmd
