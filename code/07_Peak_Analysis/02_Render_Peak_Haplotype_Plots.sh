0e2fca3e320f6d5ea3bb8446745c11c9dd726095
Jason Gallant <jgallant@msu.edu>
2024-02-28 15:59:30 -0500

    updated path

--- code/07_Peak_Analysis/02_Render_Peak_Haplotype_Plots.sh
+++ code/07_Peak_Analysis/02_Render_Peak_Haplotype_Plots.sh
@@ -11,4 +11,4 @@ outdir=${root}/output_data/07_Peak_Analysis/${OUTNAME}
 mkdir -p ${outdir}
 
 # Rscript ${scriptdir}/render_report.R INPUTDATA OUTPUTDATA REPORT.Rmd
-singularity exec ${gwas_tools_image} Rscript ${root}/code/00_common/render_rmd_report.R ${OUTNAME} ${outdir}/${OUTNAME}_Peak_Analysis ${root}/code/06_Association/peak_analysis.Rmd
+singularity exec ${gwas_tools_image} Rscript ${root}/code/00_common/render_rmd_report.R ${OUTNAME} ${outdir}/${OUTNAME}_Peak_Analysis ${root}/code/07_Peak_Analysis/peak_analysis.Rmd
