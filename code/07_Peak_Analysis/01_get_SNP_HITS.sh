fec2c3ebe1ecf7e2fbb74bd6d08f32b544d71c22
Jason Gallant <jgallant@msu.edu>
2024-02-28 16:08:31 -0500

    updated paths

--- code/07_Peak_Analysis/01_get_SNP_HITS.sh
+++ code/07_Peak_Analysis/01_get_SNP_HITS.sh
@@ -10,7 +10,7 @@
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
-outdir=${root}/output_data/07_Peak_Analysis/
+outdir=${root}/output_data/07_Peak_Analysis/${OUTNAME}
 mkdir -p ${outdir}
 
 OUTNAME=$1
@@ -21,16 +21,16 @@ TOP_SNPS=${root}/output_data/06_Association/${OUTNAME}/${OUTNAME}"_SNPS_IN_PEAKS
 
 cut -f 1,2 $TOP_SNPS > ${outdir}/${OUTNAME}_snps.txt
 
-singularity exec ${gwas_tools_image} bcftools view -R ${outdir}/${OUTNAME}_snps.txt ${root}/output_data/03_QC/$snps_only_vcf -O z -o ${outdir}/${OUTNAME}_OUTLIER_SNPs_TOP250.vcf.gz
+singularity exec ${gwas_tools_image} bcftools view -R ${outdir}/${OUTNAME}_snps.txt ${root}/output_data/03_QC/$snps_only_vcf -O z -o ${outdir}/${OUTNAME}_SNPS_IN_PEAKS_TOP250.vcf.gz
 
-singularity exec ${gwas_tools_image} bash -c "bcftools query -l ${outdir}/${OUTNAME}_OUTLIER_SNPs_TOP250.vcf.gz | tr \"\\n\" \"\\t\" > ${outdir}/${OUTNAME}_header.txt"
+singularity exec ${gwas_tools_image} bash -c "bcftools query -l ${outdir}/${OUTNAME}_SNPS_IN_PEAKS_TOP250.vcf.gz | tr \"\\n\" \"\\t\" > ${outdir}/${OUTNAME}_header.txt"
 
 echo "" >>  ${outdir}/${OUTNAME}_header.txt #add new line at end of header
 
-singularity exec ${gwas_tools_image} bash -c "bcftools query -f '[%GT\t]\n' ${outdir}/${OUTNAME}_OUTLIER_SNPs_TOP250.vcf.gz > ${outdir}/${OUTNAME}_raw.genos"
+singularity exec ${gwas_tools_image} bash -c "bcftools query -f '[%GT\t]\n' ${outdir}/${OUTNAME}_SNPS_IN_PEAKS_TOP250.vcf.gz > ${outdir}/${OUTNAME}_raw.genos"
 cat ${outdir}/${OUTNAME}_header.txt ${outdir}/${OUTNAME}_raw.genos >${outdir}/${OUTNAME}.genos
 
-singularity exec ${gwas_tools_image} bash -c "bcftools query -f '%CHROM\t %POS\t %REF\t %ALT\n' ${outdir}/${OUTNAME}_OUTLIER_SNPs_TOP250.vcf.gz > ${outdir}/${OUTNAME}_raw.pos"
+singularity exec ${gwas_tools_image} bash -c "bcftools query -f '%CHROM\t %POS\t %REF\t %ALT\n' ${outdir}/${OUTNAME}_SNPS_IN_PEAKS_TOP250.vcf.gz > ${outdir}/${OUTNAME}_raw.pos"
 
 echo -e "CHROM\tPOS\tREF\tALT" | cat - ${outdir}/${OUTNAME}_raw.pos > ${outdir}/${OUTNAME}.pos
 
