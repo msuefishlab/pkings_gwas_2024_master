9df75e5004394978862263668e69b5834f39936a
Jason Gallant <jgallant@msu.edu>
2024-04-03 19:50:21 -0400

    updated to use vcf2msa.py

--- code/04_Phylogeny_and_Admixture/08_SplitVCF_to_Fasta.sh
+++ code/04_Phylogeny_and_Admixture/08_SplitVCF_to_Fasta.sh
@@ -14,12 +14,16 @@ outdir=${root}/output_data/04_Phylogeny_And_Admixture/
 refdir=${root}/input_data/00_Reference_Genome/
 
 mkdir -p ${outdir}
-mkdir -p ${refdir}/split_gffs
+mkdir -p ${refdir}/split_regions
 
-awk -v outdir="${refdir}/split_gffs" '{print $0 >> outdir"/"$1".gff"}' ${refdir}/${gff_file}
+# Calculate lines per file for approximately 200 files
+total_lines=$(wc -l < ${refdir}/pkings_gene_regions.txt)
+lines_per_file=$((total_lines / 200 + 1))
 
-gff_files=($(find ${refdir}/split_gffs -type f -name "*.gff" -exec realpath {} \;))
-gff_file_count=${#gff_files[@]}
+# Split the file with numeric suffixes
+split -d -l "$lines_per_file" ${refdir}/pkings_gene_regions.txt ${refdir}/split_regions/chunk_ -a 3 --numeric-suffixes=1
 
-echo sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
-sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
+# This will create files named chunk_001, chunk_002, ..., each with around the same number of lines
+
+echo sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 1-200 --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
+sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 1-200 --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
