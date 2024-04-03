61e546575cd6e677a5228933f6ec655494fdcd79
Jason Gallant <jgallant@msu.edu>
2024-04-02 20:54:08 -0400

    minor bugfix

--- code/04_Phylogeny_and_Admixture/08_SplitVCF_to_Fasta.sh
+++ code/04_Phylogeny_and_Admixture/08_SplitVCF_to_Fasta.sh
@@ -21,5 +21,5 @@ awk -v outdir="${refdir}/split_gffs" '{print $0 >> outdir"/"$1".gff"}' ${refdir}
 gff_files=($(find ${refdir}/split_gffs -type f -name "*.gff" -exec realpath {} \;))
 gff_file_count=${#gff_files[@]}
 
-echo sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root},gff_files=${gff_files} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
-sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root},gff_files=${gff_files} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
+echo sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
+sbatch --job-name "SPLIT_VCFS" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_split_gffs_%a.log" -a 0-${gff_file_count} --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/split_vcf_to_genes.sb
