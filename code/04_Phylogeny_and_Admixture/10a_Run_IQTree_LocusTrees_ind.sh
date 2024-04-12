d2ae3c4de38ad84e92f424dc5db5b7f96be11fff
Jason Gallant <jgallant@msu.edu>
2024-04-12 09:51:54 -0400

    addded the ability to rerun failed jobs, bumped time and memory

--- code/04_Phylogeny_and_Admixture/10a_Run_IQTree_LocusTrees_ind.sh
+++ code/04_Phylogeny_and_Admixture/10a_Run_IQTree_LocusTrees_ind.sh
@@ -1,17 +1,24 @@
-## 02_Run_Prep_For_IQTree.sh
-## Dec 2023
-## JRG
-## Submits to queue for Phylogeny Reconstruction
-
 #!/bin/bash
 
-root="$(git rev-parse --show-toplevel)"
-source ${root}/"pkings_gwas.env"
+# 02_Run_Prep_For_IQTree.sh
+# Dec 2023
+# JRG
+# Submits to queue for Phylogeny Reconstruction
 
-mkdir -p ${root}/output_data/slurm_logs/04_Phylogeny_And_Admixture/
+root="$(git rev-parse --show-toplevel)"
+source "${root}/pkings_gwas.env"
+
+mkdir -p "${root}/output_data/slurm_logs/04_Phylogeny_And_Admixture/"
 
 alignments=("${root}/output_data/04_Phylogeny_And_Admixture/1000_rand_filtered_gene_alignments/"*.fasta)
 nalignments=${#alignments[@]}
 
-echo sbatch --job-name "PHYLO_IQTREE_LOCI" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_run_iqtree_loci_%a.log" -a 0-500 --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/run_iqtree_gene_loci_ind.sb
-sbatch --job-name "PHYLO_IQTREE_LOCI" --output ${root}"/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_run_iqtree_loci_%a.log" -a 0-500 --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/run_iqtree_gene_loci_ind.sb
+# Default array jobs range
+default_range="0-500"
+
+# If the first argument is not empty, use it as the array jobs range
+# Otherwise, use the default range
+array_jobs=${1:-$default_range}
+
+echo "sbatch --job-name \"PHYLO_IQTREE_LOCI\" --output ${root}/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_run_iqtree_loci_%a.log -a ${array_jobs} --export=root=${root} ${root}/code/04_Phylogeny_and_Admixture/run_iqtree_gene_loci_ind.sb"
+sbatch --job-name "PHYLO_IQTREE_LOCI" --output "${root}/output_data/slurm_logs/04_Phylogeny_And_Admixture/phylo_run_iqtree_loci_%a.log" -a "${array_jobs}" --export=root=${root} "${root}/code/04_Phylogeny_and_Admixture/run_iqtree_gene_loci_ind.sb"
