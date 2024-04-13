99f6e11341b78b02cc917519767cb49b2962b525
Jason Gallant <jgallant@msu.edu>
2024-04-13 09:24:55 -0400

    added code to pick 1000 random gene trees

--- code/04_Phylogeny_and_Admixture/pick_1000_random.sh
+++
@@ -1,5 +0,0 @@
-
-mkdir -p /mnt/research/efish/pkings_gwas_2024_master/output_data/04_Phylogeny_And_Admixture/1000_rand_filtered_gene_alignments
-cd /mnt/research/efish/pkings_gwas_2024_master/output_data/04_Phylogeny_And_Admixture/filtered_gene_alignments
-find . -type f -print0 | shuf -z -n 1000 | xargs -0 -I {} cp {} /mnt/research/efish/pkings_gwas_2024_master/output_data/04_Phylogeny_And_Admixture/1000_rand_filtered_gene_alignments
-
