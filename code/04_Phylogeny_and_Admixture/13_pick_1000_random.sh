52f7667a864ddd954628d5891cb0c9fc38a85f3e
Jason Gallant <jgallant@msu.edu>
2024-04-13 09:25:51 -0400

    added code to pick 1000 random gene trees

--- code/04_Phylogeny_and_Admixture/13_pick_1000_random.sh
+++ code/04_Phylogeny_and_Admixture/13_pick_1000_random.sh
@@ -3,7 +3,7 @@ source ${root}/"pkings_gwas.env"
 
 mkdir -p ${root}/output_data/04_Phylogeny_And_Admixture/1000_rand_gene_trees
 
-cd ${root}/output_data/0/04_Phylogeny_And_Admixture/gene_trees/
+cd ${root}/output_data/04_Phylogeny_And_Admixture/gene_trees/
 
 find . -type f -name "*.treefile" -print0 | shuf -z -n 1000 | xargs -0 -I {} cp {} ${root}/output_data/04_Phylogeny_And_Admixture/1000_rand_gene_trees
 
