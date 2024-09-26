a9910727e0868bceb26757cbd5ee2cb2384ee0e1
jasongallant <jgallant@msu.edu>
2024-09-25 20:55:28 -0400

    updates for phylo report

--- code/04_Phylogenomics/08_Render_PHYLO_report.sh
+++ code/04_Phylogenomics/08_Render_PHYLO_report.sh
@@ -6,10 +6,12 @@ source $root/pkings_gwas.env
 
 MYNAME=$(whoami)
 
-outdir=${root}/output_data/04_Phylogeny_And_Admixture/
+outdir=${root}/output_data/04_Phylogenomics/
 
 mkdir -p ${outdir}
 
 OUTROOT=${snps_only_vcf%".vcf.gz"}
 
-Rscript ${root}/code/00_common/render_rmd_report.R NULL ${outdir}/${OUTROOT}_phylogeny ${root}/code/04_Phylogeny_and_Admixture/phylogeny_and_character_reconstruction_chronos_ultrametric.Rmd
+Rscript ${root}/code/00_common/render_rmd_report.R NULL ${outdir}/${OUTROOT}_mormyrid_phylogeny ${root}/code/04_Phylogenomics/phylogeny_and_character_reconstruction_mormyrids.Rmd
+
+Rscript ${root}/code/00_common/render_rmd_report.R NULL ${outdir}/${OUTROOT}_paramormyrops_phylogeny ${root}/code/04_Phylogenomics/phylogeny_and_character_reconstruction_paramormyrops.Rmd
