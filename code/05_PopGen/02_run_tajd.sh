64f030269ca3f54c5d1302559464c0538ad2963f
jasongallant <jgallant@msu.edu>
2024-09-30 09:39:42 -0400

    updates for popgen

--- code/05_PopGen/02_run_tajd.sh
+++ code/05_PopGen/02_run_tajd.sh
@@ -8,9 +8,16 @@
 root="$(git rev-parse --show-toplevel)"
 source ${root}/"pkings_gwas.env"
 
-interval_files=($(find "${root}/input_data/01_Terra/chrom-interval-files/" "${root}/input_data/01_Terra/remaining-interval-files/" -type f))
+intervals=()
 
-nintervals=${#interval_files[@]}
+# Use a while loop to read the file line by line
+while IFS= read -r line; do
+    first_column=$(echo "$line" | awk '{print $1}')
+    # Append the first column to the array
+    intervals+=("$first_column")
+done < "${root}/input_data/00_Reference_Genome/${reference}.fai"
+
+nintervals=${#intervals[@]}
 
 mkdir -p $root"/output_data/slurm_logs"/05_PopGen/
 
