fee1bbd282458ba1e1bf6729d094d455589d7f49
Jason Gallant <jgallant@msu.edu>
2024-10-17 14:21:56 -0400

    addded gwas report renderer

---
+++ code/00_common/render_gwas_report.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+
+# run this script like this:
+# Rscript ${scriptdir}/render_report.R INPUTDATA OUTPUTDATA REPORT.Rmd MAF_THRESHOLD
+
+args <- commandArgs(trailingOnly = TRUE)
+
+root <- rprojroot::find_root(".git/index")
+require(yaml)
+require(tools)
+
+
+rmarkdown::render(
+    input = file.path(args[3]),
+    output_file = basename(paste0(args[2], ".report.html")),
+    output_dir = dirname(args[2]),
+    params = list(
+        data_path = args[1],
+        MAF_thresh = args[4]
+    )
+)
