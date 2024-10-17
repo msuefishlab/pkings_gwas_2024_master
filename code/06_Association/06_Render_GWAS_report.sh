4b0ce198bd152c3e6d44900af5907786e08adb0f
Jason Gallant <jgallant@msu.edu>
2024-10-17 14:23:21 -0400

    added gwas report with maf filtering

--- code/06_Association/06_Render_GWAS_report.sh
+++ code/06_Association/06_Render_GWAS_report.sh
@@ -2,6 +2,8 @@
 
 OUTNAME=$1
 
+MAF_THRESH=$2
+
 root="$(git rev-parse --show-toplevel)"
 
 source $root/pkings_gwas.env
@@ -11,4 +13,4 @@ outdir=${root}/output_data/06_Association/${OUTNAME}
 mkdir -p ${outdir}
 
 # Rscript ${scriptdir}/render_report.R INPUTDATA OUTPUTDATA REPORT.Rmd
-singularity exec ${gwas_tools_image} Rscript ${root}/code/00_common/render_rmd_report.R ${OUTNAME} ${outdir}/${OUTNAME}_GWAS_Stage1 ${root}/code/06_Association/gwas_report.Rmd
+singularity exec ${gwas_tools_image} Rscript ${root}/code/00_common/render_gwas_report.R ${OUTNAME} ${outdir}/${OUTNAME}_GWAS_Stage1 ${root}/code/06_Association/gwas_report.Rmd ${MAF_THRESH}
