048b666dab88e71e25865cde3c6c6089400646b6
jasongallant <jgallant@msu.edu>
2024-10-18 16:53:36 -0400

    updated phasing pipeline

---
+++ code/08_Phasing/02_create_sample_batches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## 01_create_region_lists.sh
+## Feb 2023
+## JRG
+# This script creates region files for downstream phasing and is run interactively from the command line.
+
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
+
+outdir=${root}/input_data/08_Phasing/
+mkdir -p ${outdir}
+
+module purge; module load BCFtools
+
+bcftools query -l output_data/03_QC/${snps_only_for_phasing} | split -l 25 -d - input_data/08_Phasing/sample_names_b
+
--- code/08_Phasing/03_whatshap_submit.sh
+++ code/08_Phasing/03_whatshap_submit.sh
@@ -6,24 +6,20 @@
 # This script creates runs whatshap on sample batches located in the metadata folder
 # e.g. bash 03_whatshap_submit.sh sample_names_b_0.txt
 
-scriptdir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
-WORK_D=$(git rev-parse --show-toplevel)
+root="$(git rev-parse --show-toplevel)"
+source ${root}/"pkings_gwas.env"
 
-cd $WORK_D
+outdir=${root}/input_data/08_Phasing/
+mkdir -p ${outdir}
 
-echo $scriptdir
-
-source $WORK_D/code/00_utility/parse_yaml.sh
-
-eval $(parse_yaml ./global_params.yaml)
+mkdir -p ${root}/output_data/slurm_logs/08_Phasing/
 
 while read SAMPLE
 do
-  cd $(git rev-parse --show-toplevel)
   echo "$SAMPLE"
-  mkdir -p ./output_data/whatshap/$SAMPLE
-  cd ./output_data/whatshap/$SAMPLE
-  mkdir -p $WORK_D/slurm_outputs/whatshap
-  sbatch --job-name $SAMPLE"_WHATSHAP" --array=0-25 --output $WORK_D/slurm_outputs/whatshap/$SAMPLE"_WHATSHAP.slurm_%j_%a.log" --export=scriptdir=$scriptdir $scriptdir/submit_whatshap.sb $SAMPLE
+  mkdir -p ${outdir}/$SAMPLE
+
+  echo sbatch --job-name ${SAMPLE}_WHATSHAP --array=0-25 --output ${root}/output_data/slurm_logs/08_Phasing/${SAMPLE}_WHATSHAP.slurm_%a.log --export=root=${root} ${root}/code/08_Phasing/submit_whatshap.sb $SAMPLE
+  sbatch --job-name ${SAMPLE}_WHATSHAP --array=0-25 --output ${root}/output_data/slurm_logs/08_Phasing/${SAMPLE}_WHATSHAP.slurm_%a.log --export=root=${root} ${root}/code/08_Phasing/submit_whatshap.sb $SAMPLE
 
 done < "$1"
