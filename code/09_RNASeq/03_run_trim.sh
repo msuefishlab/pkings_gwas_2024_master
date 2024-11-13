9c48291ec5cde8318ce17c7854e8c9c80668df26
jasongallant <jgallant@msu.edu>
2024-11-13 10:28:41 -0500

    updates to RNAseq pipeline

--- code/09_RNASeq/03_run_trim.sh
+++ code/09_RNASeq/03_run_trim.sh
@@ -8,15 +8,25 @@ source "$root/pkings_gwas.env"
 # Define the input file
 input_file=$1
 
+readsdir="${root}/output_data/09_RNASeq/raw_reads"  # Define the output director
 outdir="${root}/output_data/09_RNASeq/trimmed_reads"  # Define the output director
+slurmoutdir=${root}/output_data/slurm_outputs/09_RNASeq
 
+mkdir -p $outdir
+mkdir -p $slurmoutdir
 
-# Skip the header and read the file line by line using a file descriptor
 {
   read -r header # Skip the header
+  declare -A seen_specnos # Declare an associative array to track unique specnos
+
   while IFS=$'\t' read -r specno population r1 r2 phenotype; do
-    mkdir -p ${outdir}/${specno};
-    echo sbatch --output=${sample}-trim-%j.out --export=READ1="${i}",READ2="${i//R1/R2}",OUTNAME=$SCRATCH/pkings_trimmed/${sample},SAMPLE=${sample} run_trim.sb;
-    sbatch --output=${sample}-trim-%j.out --export=READ1="${i}",READ2="${i//R1/R2}",OUTNAME=$SCRATCH/pkings_trimmed/${sample},SAMPLE=${sample} run_trim.sb;
+    if [[ -z "${seen_specnos[$specno]}" ]]; then
+      # If specno is not yet seen
+      seen_specnos[$specno]=1 # Mark specno as seen
+      mkdir -p "${outdir}/${specno}"
+      echo sbatch --output="${slurmoutdir}/trim_${specno}.out" --export=READ1="${readsdir}/${specno}_R1_combined.fastq.gz",READ2="${readsdir}/${specno}_R2_combined.fastq.gz",OUTNAME="${outdir}/${specno}",SAMPLE="${specno}" "${root}/code/09_RNASeq/run_trim.sb"
+      sbatch --output="${slurmoutdir}/trim_${specno}.out" --export=READ1="${readsdir}/${specno}_R1_combined.fastq.gz",READ2="${readsdir}/${specno}_R2_combined.fastq.gz",OUTNAME="${outdir}/${specno}",SAMPLE="${specno}" "${root}/code/09_RNASeq/run_trim.sb"
+    fi
   done
 } < "$input_file"
+
--- code/09_RNASeq/05_run_rsem_quantify.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ./RSEM
-
-for i in stage1/*Aligned.toTranscriptome.out.bam
-do
-  sample=$(basename ${i//Aligned.toTranscriptome.out.bam/});
-  echo sbatch --output=${sample}-RSEM-%j.out --export=OUTNAME=${sample} run_rsem.sb;
-  sbatch --output=${sample}-RSEM-%j.out --export=OUTNAME=${sample} run_rsem.sb;
- done
