f3545566b7ee547e5b8b77174038c0f1c0a66799
Jason Gallant <jgallant@msu.edu>
2025-08-29 11:28:35 -0400

    updated rmats filtering

--- code/09_RNASeq/09_filter_rmats.sh
+++ code/09_RNASeq/09_filter_rmats.sh
@@ -1,12 +1,13 @@
-sc_rmats_filter=$(pwd)/rmats_filtering.py
+source pkings_gwas.env
+sc_rmats_filter=${root}/code/09_RNAseq/rmats_filtering.py
 declare -a event_array=("SE" "MXE" "RI" "A5SS" "A3SS")
 declare -a counttype_array=("JC" "JCEC")
 
-mkdir -p ../../output_data/09_RNASeq/rmats_filtered/gw1/
-cd ../../output_data/09_RNASeq/rmats_filtered/gw1/
+mkdir -p ${root}/output_data/09_RNASeq/rmats_filtered/gw2/
+cd ${root}/output_data/09_RNASeq/rmats_filtered/gw2/
 
 for event in "${event_array[@]}"; do
     for counttype in "${counttype_array[@]}"; do
-        python $sc_rmats_filter ../../rmats/gw1/post/${event}.MATS.${counttype}.txt 10,0.05,0.95,0.01,0.05,0.5,0.2
+        python $sc_rmats_filter ../../rmats/gw2/post/${event}.MATS.${counttype}.txt 10,0.05,0.95,0.01,0.05,0.5,0.2
     done
 done
